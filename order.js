7ec3181c7384249d6f597c55c36cd7e8075dd20b
szaboger <szboger11@gmail.com>
2020-06-30 21:50:54 +0200

    New panel

--- order.js
+++ order.js
@@ -5,5 +5,5 @@ function getInfo2() {
     var darab = document.getElementById("darabszamrendeles").value
     var hamburgerrendeles = document.getElementById("hamburgerrendeles").value
     var extrafeltet = document.getElementById("extrafeltet").value
-    console.log("a te e-mail címed " + emailrendeles + " a neved pedig " + nev + "a lakcímed: " + lakcim + " " + darab + "-ot rendeltél" + " " + hamburgerrendeles + " fajta hamburgert rendeltél és " + etrafeltet + "-et kértél"  )
+    console.log("a te e-mail címed " + emailrendeles + " a neved pedig " + nev + "a lakcímed: " + lakcim + " " + darab + "-ot rendeltél" + " " + hamburgerrendeles + " fajta hamburgert rendeltél és " + extrafeltet + "-et kértél"  )
 }
