376fde2177b7bb619efc108719e544ee446208c3
szaboger <szboger11@gmail.com>
2020-07-02 11:22:38 +0200

    BackEND

---
+++ action.php
@@ -0,0 +1,13 @@
+<?php
+ $path = 'data.txt';
+ if (isset($_POST['field1']) && isset($_POST['field2'])) {
+    $fh = fopen($path,"a+");
+    $string = $_POST['field1'].' - '.$_POST['field2'];
+    fwrite($fh,$string); // Write information to the file
+    fclose($fh); // Close the file
+ }
+ 
+    header("Location: http://localhost/githuby.html"); /* Redirect browser */
+    
+
+?>
